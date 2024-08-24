12eb9c74831236beb8508cc3d899fd3c57f7dae4
Reagan Lopez <reagan.lopez@intel.com>
2024-08-24 07:19:41 -0700

    Refactor
    Signed-off-by: Reagan Lopez <reagan.lopez@intel.com>

--- experiments/qdrant_multitenancy.py
+++ experiments/qdrant_multitenancy.py
@@ -1,76 +1,78 @@
 from qdrant_client import QdrantClient, models
 
-client = QdrantClient(":memory:")
-
+# VectorDB parameters
+location = ":memory:"
 collection_name = "my_kb"
-client.create_collection(
-    collection_name=collection_name,
-    vectors_config=models.VectorParams(size=3, distance=models.Distance.COSINE),
-    hnsw_config=models.HnswConfigDiff(
-        payload_m=16,
-        m=0,
-    ),
-)
+vector_size = 3
+distance_metric = models.Distance.COSINE
+group_id = "kb_id"
+response_limit = 1
 
-client.create_payload_index(
-    collection_name=collection_name,
-    field_name="kb_id",
-    field_schema=models.KeywordIndexParams(
-        type="keyword",
-        is_tenant=True,
-    ),
-)
+client = QdrantClient(location=location)
 
 
-client.upsert(
-    collection_name=collection_name,
-    points=[
-        models.PointStruct(
-            id=1,
-            payload={"kb_id": "1"},
-            vector=[0.9, 0.1, 0.1],
+def query_vector_in_group(gid, query_vector):
+    search_result = client.query_points(
+        collection_name=collection_name,
+        query=query_vector,
+        query_filter=models.Filter(
+            must=[
+                models.FieldCondition(
+                    key=group_id,
+                    match=models.MatchValue(
+                        value=gid,
+                    ),
+                )
+            ]
         ),
-        models.PointStruct(
-            id=2,
-            payload={"kb_id": "1"},
-            vector=[0.1, 0.9, 0.1],
-        ),
-        models.PointStruct(
-            id=3,
-            payload={"kb_id": "2"},
-            vector=[0.1, 0.1, 0.9],
-        ),
-    ],
-)
+        limit=response_limit,
+    ).points
+    print(search_result)
 
-search_result = client.query_points(
-    collection_name=collection_name,
-    query_filter=models.Filter(
-        must=[
-            models.FieldCondition(
-                key="kb_id",
-                match=models.MatchValue(
-                    value="1",
-                ),
-            )
-        ]
-    ),
-    limit=1,
-).points
-print(search_result)
 
-search_result = client.query_points(
-    collection_name=collection_name,
-    query_filter=models.Filter(
-        must=[
-            models.FieldCondition(
-                key="kb_id",
-                match=models.MatchValue(
-                    value="2",
-                ),
-            )
-        ]
-    ),
-    limit=1,
-).points
-print(search_result)
+def main():
+    client.create_collection(
+        collection_name=collection_name,
+        vectors_config=models.VectorParams(size=vector_size, distance=distance_metric),
+        hnsw_config=models.HnswConfigDiff(
+            payload_m=16,
+            m=0,
+        ),
+    )
+
+    client.create_payload_index(
+        collection_name=collection_name,
+        field_name=group_id,
+        field_schema=models.KeywordIndexParams(
+            type="keyword",
+            is_tenant=True,
+        ),
+    )
+
+    client.upsert(
+        collection_name=collection_name,
+        points=[
+            models.PointStruct(
+                id=1,
+                payload={group_id: "1"},
+                vector=[0.9, 0.1, 0.1],
+            ),
+            models.PointStruct(
+                id=2,
+                payload={group_id: "1"},
+                vector=[0.1, 0.9, 0.1],
+            ),
+            models.PointStruct(
+                id=3,
+                payload={group_id: "2"},
+                vector=[0.1, 0.1, 0.9],
+            ),
+        ],
+    )
+
+    query_vector_in_group("1", [0.9, 0.1, 0.1])
+    query_vector_in_group("2", [0.9, 0.1, 0.1])
+
+
+if __name__ == "__main__":
+    main()
